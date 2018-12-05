e66e6bebc0c333a6dfd5eea90e5c3e4035e61f87
Serhii PERKHUN <sperkhun@e1r11p8.unit.ua>
2018-12-05 11:25:24 +0200

    Initial Commit

---
+++ axon_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  axon_test
+//
+//  Created by Serhii PERKHUN on 12/5/18.
+//  Copyright © 2018 Serhii PERKHUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
